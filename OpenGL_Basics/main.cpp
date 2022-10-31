b8536feb6a363b7484fa0f5a90ba3bb19c5ae6ef
Jaibeer  S Dugal <jaibeer72@gmail.com>
2022-10-31 11:06:48 +0000

    Initial Commit

---
+++ OpenGL_Basics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  OpenGL_Basics
+//
+//  Created by Jaibeer Dugal on 31/10/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
