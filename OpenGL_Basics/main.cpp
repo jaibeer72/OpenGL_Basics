092ece8e089304de3e6dfc07989328af540e97d3
Jaibeer  S Dugal <jaibeer72@gmail.com>
2022-10-31 11:27:31 +0000

    project setup

--- OpenGL_Basics/main.cpp
+++ OpenGL_Basics/main.cpp
@@ -7,6 +7,12 @@
 
 #include <iostream>
 
+#include <glad/glad.h>
+#include <glfw/glfw3.h>
+
+
+
+
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
