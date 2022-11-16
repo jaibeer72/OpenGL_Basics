40cc77ecee8bc33afaf71b0d3dbbd12d85607784
Jaibeer  S Dugal <jaibeer72@gmail.com>
2022-11-16 13:57:17 +0000

    working on lighting objects

---
+++ OpenGL_Basics/RenderableObjctsBase/Objects/LitObjects/Lit_UnitCube.cpp
@@ -0,0 +1,8 @@
+//
+//  Lit_UnitCube.cpp
+//  OpenGL_Basics
+//
+//  Created by Jaibeer Dugal on 16/11/2022.
+//
+
+#include "Lit_UnitCube.hpp"
---
+++ OpenGL_Basics/RenderableObjctsBase/Objects/LitObjects/Lit_UnitCube.hpp
@@ -0,0 +1,13 @@
+//
+//  Lit_UnitCube.hpp
+//  OpenGL_Basics
+//
+//  Created by Jaibeer Dugal on 16/11/2022.
+//
+
+#ifndef Lit_UnitCube_hpp
+#define Lit_UnitCube_hpp
+
+#include <stdio.h>
+
+#endif /* Lit_UnitCube_hpp */
